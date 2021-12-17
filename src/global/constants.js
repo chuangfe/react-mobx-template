a1b3c45f319ea28ce2b6105bcb087a285cd5338a
Chuang YU <chuangfe@gmail.com>
2021-12-17 15:27:31 +0800

    Update constaints

---
+++ src/global/constants.js
@@ -0,0 +1,229 @@
+export const questionsClassX = [
+  {
+    id: 1,
+    class: 'x',
+    question: '你在陌生的國度醒來，肌膚感受這裡的氣候是....',
+    answers: [
+      {
+        fraction: 0,
+        content: '耀眼南島陽光海岸，白沙上海風輕拂'
+      },
+      {
+        fraction: 1,
+        content: '薄霧繚繞中海拔森林，青苔泥土氣息'
+      }
+    ]
+  },
+
+  {
+    id: 2,
+    class: 'x',
+    question: '還來不及睜開眼，就先聞到大地物產的香氣襲來...',
+    answers: [
+      {
+        fraction: 0,
+        content: '焦糖、可可、鳳梨果乾、煙燻甘草香氣'
+      },
+      {
+        fraction: 1,
+        content: '百香果、芬多精、淡淡蜜桃、白色花香氣'
+      }
+    ]
+  },
+
+  {
+    id: 3,
+    class: 'x',
+    question: '終於願意醒來，在這屬於自己的時光裡享用的甜點，會是...',
+    answers: [
+      {
+        fraction: 0,
+        content: '濃郁流心熔岩的布朗尼'
+      },
+      {
+        fraction: 1,
+        content: '橙花糖霜的水果冰淇淋'
+      }
+    ]
+  },
+
+  {
+    id: 4,
+    class: 'x',
+    question: '遠處傳來悠悠的旋律，你覺得是...',
+    answers: [
+      {
+        fraction: 0,
+        content: '渾厚嗓音，大山子民對山林表達的敬畏'
+      },
+      {
+        fraction: 1,
+        content: '清澈提琴聲，輕巧的旋律蔓延整座天際'
+      }
+    ]
+  },
+
+  {
+    id: 5,
+    class: 'x',
+    question: '朝著聲音方向前進的路上，口渴了，為自己倒一杯茶飲...',
+    answers: [
+      {
+        fraction: 0,
+        content: '倒在陶土材質的淺杯裡，冒著裊裊熱氣'
+      },
+      {
+        fraction: 1,
+        content: '澆淋在裝滿冰塊的玻璃杯，冰塊清脆碎裂'
+      }
+    ]
+  },
+
+  {
+    id: 6,
+    class: 'x',
+    question: '啜一口茶湯，與舌尖相遇的質感是',
+    answers: [
+      {
+        fraction: 0,
+        content: '渾厚濃郁、奶油般綿密質地'
+      },
+      {
+        fraction: 1,
+        content: '輕盈水感、果汁般清爽質地'
+      }
+    ]
+  },
+
+  {
+    id: 7,
+    class: 'x',
+    question: '這樣的氣氛讓你感到心情安定，現在的情緒是什麼顏色呢...',
+    answers: [
+      {
+        fraction: 0,
+        content: '秋葉緩緩落下的紅褐色調'
+      },
+      {
+        fraction: 1,
+        content: '春風吹拂搖擺的青綠色調'
+      }
+    ]
+  }
+];
+
+export const questionsClassY = [
+  {
+    id: 8,
+    class: 'y',
+    question: '忽然有一隻小動物停在身邊，思緒被拉了回來，你覺得牠是...',
+    answers: [
+      {
+        fraction: 0,
+        content: '安穩吃草圓滾滾的野兔'
+      },
+      {
+        fraction: 1,
+        content: '雀躍跳動小小的綠繡眼'
+      }
+    ]
+  },
+
+  {
+    id: 9,
+    class: 'y',
+    question: '抵達了旋律的源頭，有兩個寶箱，上面寫著對你的祝福...',
+    answers: [
+      {
+        fraction: 0,
+        content: '願你每個工作日裡，都能輕快忙裡偷閒'
+      },
+      {
+        fraction: 1,
+        content: '願你每個假日時光，都有開心豐富體驗'
+      }
+    ]
+  },
+
+  {
+    id: 10,
+    class: 'y',
+    question: '寶箱裡裝的原來是一瓶神奇藥水，你覺得它可以...',
+    answers: [
+      {
+        fraction: 0,
+        content: '給你舒緩放鬆的平靜'
+      },
+      {
+        fraction: 1,
+        content: '給你精力充沛的能量'
+      }
+    ]
+  }
+];
+
+export const personalityTraits = [
+  {
+    fractionMinX: 0,
+    fractionMaxX: 1,
+    fractionMinY: 2,
+    fractionMaxY: 3,
+    answer: '大山大海的子民'
+  },
+
+  {
+    fractionMinX: 0,
+    fractionMaxX: 1,
+    fractionMinY: 0,
+    fractionMaxY: 1,
+    answer: '有故事的溫柔職人'
+  },
+
+  {
+    fractionMinX: 2,
+    fractionMaxX: 3,
+    fractionMinY: 2,
+    fractionMaxY: 3,
+    answer: '天真無邪的自由小孩'
+  },
+
+  {
+    fractionMinX: 2,
+    fractionMaxX: 3,
+    fractionMinY: 0,
+    fractionMaxY: 1,
+    answer: '奢華古典的皇室成員'
+  },
+
+  {
+    fractionMinX: 4,
+    fractionMaxX: 5,
+    fractionMinY: 2,
+    fractionMaxY: 3,
+    answer: '散發魅力的鬼靈精'
+  },
+
+  {
+    fractionMinX: 4,
+    fractionMaxX: 5,
+    fractionMinY: 0,
+    fractionMaxY: 1,
+    answer: '山林裡的個性智者'
+  },
+
+  {
+    fractionMinX: 6,
+    fractionMaxX: 7,
+    fractionMinY: 2,
+    fractionMaxY: 3,
+    answer: '擁有銀鈴笑聲的小仙子'
+  },
+
+  {
+    fractionMinX: 6,
+    fractionMaxX: 7,
+    fractionMinY: 0,
+    fractionMaxY: 1,
+    answer: '甜美如蜜的小不點'
+  }
+];
